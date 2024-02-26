1ca2dfa42ee3f3af0c640fabdd1aecddcac9d813
torre <rossilandress@gmail.com>
2024-02-26 17:29:02 -0600

    difl

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+//Ejercicio#3
+    //Escribir un programa que calcule e imprima la suma de los primeros 100 números naturales.
+
+    int suma = 0;
+    for (int i = 1; i <= 100; i++) {
+        suma += i;
+    }
+    std::cout << "La suma de los primeros 100 números naturales es: " << suma <<endl;
+    return 0;
+}
